44facbcd24ae89724cb1aaefb37e8ac7a95e2b30
ki-sd <redringo4@gmail.com>
2026-06-12 17:46:53 +0900

    feat: 로그인 Model 설계

--- src/main/java/com/kisd/model/EmpModel.java
+++ src/main/java/com/kisd/model/EmpModel.java
@@ -1,10 +1,11 @@
 package com.kisd.model;
 
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.util.*;
 import com.kisd.vo.*;
 import com.kisd.dao.*;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class EmpModel {
     public void empList(HttpServletRequest request){
@@ -20,6 +21,33 @@ public class EmpModel {
         request.setAttribute("total",total);
         request.setAttribute("list",list);
         request.setAttribute("start",start);
+
+    }
+    public void empLogin(HttpServletRequest request, HttpServletResponse response){
+        String id=request.getParameter("id");
+        String pwd=request.getParameter("pwd");
+        EmpVO vo=EmpDAO.empLoginData(id,pwd);
+        Boolean isLogin=false;
+        String state="";
+        String msg = (vo.getMsg() != null) ? vo.getMsg() : "";
+        switch (msg) {
+            case "OK"    -> {
+                isLogin=true;
+            }
+            case "NOID"  -> {
+                state="NOID";
+            }
+            case "NOPWD" -> {
+                state="NOPWD";
+            }
+            default      -> {
+                state="UNKNOWN";
+            }
+        }
+        HttpSession session= request.getSession();
+        session.setAttribute("isLogin",isLogin);
+        session.setAttribute("vo",vo);
+        request.setAttribute("state",state);
 
     }
 }
--- src/main/webapp/jsp/views/cust/login.jsp
+++ src/main/webapp/jsp/views/cust/login.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: sist8
-  Date: 2026-06-11
-  Time: 오전 8:59
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-
+    
 </body>
 </html>
