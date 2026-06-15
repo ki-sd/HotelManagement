a201ae55bb56d887f437626916c619f65c24921e
ki-sd <redringo4@gmail.com>
2026-06-15 17:35:21 +0900

    feat: 로그인 form 제작 및 Modal로 재설계

--- src/main/java/com/kisd/model/EmpModel.java
+++ src/main/java/com/kisd/model/EmpModel.java
@@ -34,10 +34,8 @@ public class EmpModel {
         String id = request.getParameter("id");
         String pwd = request.getParameter("pwd");
 
-        // 1. DB에서 회원 정보 조회
         EmpVO vo = EmpDAO.empLoginData(id, pwd);
 
-        // 2. 상태 코드 판별
         String state = "UNKNOWN";
         if(vo != null && vo.getMsg() != null){
             switch (vo.getMsg()) {
@@ -48,14 +46,12 @@ public class EmpModel {
         }
 
         try {
-            // 3. 로그인 성공 시에만 세션 발급
             if("OK".equals(state)) {
                 HttpSession session = request.getSession();
                 session.setAttribute("isLogin", true);
                 session.setAttribute("user", vo);
             }
 
-            // ⭕ 4. [핵심] 화면 주소가 아니라, 상태 코드("OK", "NOID" 등)를 텍스트로 직접 출력해서 AJAX에게 쏴줍니다.
             response.setContentType("text/plain;charset=UTF-8");
             PrintWriter out = response.getWriter();
             out.write(state);
@@ -65,7 +61,6 @@ public class EmpModel {
             ex.printStackTrace();
         }
 
-        // ⭕ 5. [핵심] null을 리턴하면 프론트 컨트롤러(DispatcherServlet)가 화면 이동을 생략하고 제자리에 멈춥니다.
         return null;
     }
 }
